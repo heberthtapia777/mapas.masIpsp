780fd375049cc0b727cf283bf72790fc4699f9c8
Heberth <hb-tapia@outlook.es>
2019-08-26 14:21:51 -0400

    Actualizando archivos
    Corregui los errores en los archivos ahora tiene que correr

---
+++ adodb5/tests/_vti_cnf/test.php
@@ -0,0 +1,19 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|29 Apr 2014 23:42:20 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|VAIO\\SONY
+vti_modifiedby:SR|VAIO\\SONY
+vti_timecreated:TR|29 Apr 2014 23:42:20 -0000
+vti_cacheddtm:TX|29 Apr 2014 23:42:20 -0000
+vti_filesize:IR|56014
+vti_cachedtitle:SR|ADODB Testing
+vti_cachedbodystyle:SR|<body bgcolor=white>
+vti_cachedlinkinfo:VX|H|http://adodb.sourceforge.net H|test4.php H|testsessions.php H|testpaging.php H|test-perf.php
+vti_cachedsvcrellinks:VX|NHHS|http://adodb.sourceforge.net FHUS|adodb5/tests/test4.php FHUS|adodb5/tests/testsessions.php FHUS|adodb5/tests/testpaging.php FHUS|adodb5/tests/test-perf.php
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_charset:SR|utf-8
+vti_title:SR|ADODB Testing
+vti_backlinkinfo:VX|
